9eed09262091d9b05ba89039c03ce31443312d8d
miguelangel <miguelangpoveda@gmail.com>
2025-05-11 23:24:43 -0500

    APP-006 - ajuete de boton regresar

--- resources/views/verislife/verificacionPlan.blade.php
+++ resources/views/verislife/verificacionPlan.blade.php
@@ -190,7 +190,7 @@ Registro
                     </div>
                 </div>
                 <div class="d-flex gap-3 justify-content-center">
-                    <a href="#!" class="btn btn-outline-cerulean-blue-800"><i class="fa-solid fa-chevron-left me-2"></i> Regresar</a>
+                    <a href="/verislife/home" class="btn btn-outline-cerulean-blue-800"><i class="fa-solid fa-chevron-left me-2"></i> Regresar</a>
                     <button type="submit" form="verificacionPlanForm" class="btn btn-cerulean-blue-800">Continuar <i class="fa-solid fa-chevron-right ms-2"></i></button>
                 </div>
             </div>
