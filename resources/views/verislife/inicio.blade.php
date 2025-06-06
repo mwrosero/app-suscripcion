aa1ec1fa3952c2b142e65b8b021d5ad3a26d1d98
Michael Rosero <michael.rosero@akold.com>
2025-06-06 12:37:09 -0500

    ajuste

--- resources/views/verislife/inicio.blade.php
+++ resources/views/verislife/inicio.blade.php
@@ -879,7 +879,7 @@ $processId = base64_encode(uniqid());
         const baseUrl = `${api_url}/empresarial/v1/suscripcion/planes/detalle_empresa`; 
         const queryParams = new URLSearchParams({
             estado: 'ACTIVO',
-            frecuencia: 'ANUAL',
+            frecuencia: 'TODOS',
             lineaNegocio: 'CMV',
             codigoCliente: codigoCliente
         });
@@ -932,7 +932,7 @@ $processId = base64_encode(uniqid());
                                 <h5 class="option-title fw-medium mb-0">${plan.nombre}</h5>
                             </div>
                             <div class="price-block text-start">
-                                <h5 class="fw-semibold mb-0">$${plan.valorFinal.toFixed(2)} <small class="fw-normal fs-6">/anual</small></h5>
+                                <h5 class="fw-semibold mb-0">$${plan.valorFinal.toFixed(2)} <small class="fw-normal fs-6">/${plan.tipo}</small></h5>
                                 <p class="text-fiord-700 text-decoration-line-through fs-12p mb-0">PVP $${plan.precio.toFixed(2)}</p>
                             </div>
                         </div>
@@ -949,7 +949,7 @@ $processId = base64_encode(uniqid());
                             <span class="badge rounded-pill bg-blue-ribbon-600 text-white fw-normal" style="font-size: 0.625rem;">AHORRA ${plan.porcentajeDescuento}%</span>
                         </div>
                         <div class="my-1">
-                            <h1 class="fw-bold text-blue-zodiac-950 m-0">$${plan.valorFinal.toFixed(2)} <small class="fw-medium fs-5">/anual</small></h1>
+                            <h1 class="fw-bold text-blue-zodiac-950 m-0">$${plan.valorFinal.toFixed(2)} <small class="fw-medium fs-5">/${plan.tipo}</small></h1>
                             <small class="text-muted text-decoration-line-through text-xs">PVP: $${plan.precio.toFixed(2)}</small>
                         </div>
                     </div>
