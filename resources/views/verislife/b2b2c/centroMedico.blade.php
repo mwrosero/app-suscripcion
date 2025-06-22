72afaad00e6c2557242d85e0f8dbb46b276d576a
miguelangel <miguelangpoveda@gmail.com>
2025-06-22 11:33:10 -0500

    APP-021 card centro medico efecto hover

--- resources/views/verislife/b2b2c/centroMedico.blade.php
+++ resources/views/verislife/b2b2c/centroMedico.blade.php
@@ -13,7 +13,7 @@ VerisLife - Selecciona la empresa
             <div class="card bg-cerulean-blue-800 border-0 rounded-0 h-100">
                 <div class="card-body">
                     <div class="d-flex flex-column justify-content-center h-100">
-                        <h3 class="text-white mb-1">Te cuidamos con</h3>
+                        <h3 class="text-white mt-5 mb-1">Te cuidamos con</h3>
                         <div class="bg-zumthor-50 text-primary-veris text-center rounded-3 h3 p-2">más beneficios</div>
                         <img src="{{ request()->getHost() === '127.0.0.1' ? url('/') : secure_url('/') }}/images/illustration/veris/injury.svg" alt="Beneficios" class="img-fluid mt-auto" style="max-height: 352px;">
                     </div>
@@ -23,34 +23,34 @@ VerisLife - Selecciona la empresa
 
         <!-- Right side -->
         <div class="right-panel">
-            <div class="text-center w-100">
+            <div class="text-center w-100 mt-4">
                 <h4 class="text-fiord-700 fw-semibold mb-2">Hola <span class="text-primary-veris">(nombre del usuario)</span>,</h4>
                 <h5 class="text-fiord-700 fw-medium mb-4">Te damos la bienvenida a Veris care</h5>
                 <h5 class="fw-medium text-primary-veris mb-5">Escoge el programa de fidelización a tu medida:</h5>
 
                 <div class="row gap-4 justify-content-center">
                     <div class="col-12 col-md-4">
-                        <div class="card shadow-1 rounded-4 h-100 card-hover text-center">
+                        <div class="card shadow-1 rounded-4 card-hover text-center">
                             <div class="card-body position-relative">
-                                <img src="{{ request()->getHost() === '127.0.0.1' ? url('/') : secure_url('/') }}/assets/img/veris/icono-veris-vertical.svg" alt="Veris" class="logo-veris mb-3" />
-                                <div class="card-info">
+                                <img src="{{ request()->getHost() === '127.0.0.1' ? url('/') : secure_url('/') }}/assets/img/veris/icono-veris-vertical-lg.svg" class="my-auto" alt="Veris"/>
+                                <div class="card-info pt-5 p-4">
                                     <div class="badge-descuento">-15% en:</div>
-                                    <div class="row g-2 justify-content-center">
+                                    <div class="row g-0 justify-content-center">
                                         <div class="col-6 col-md-6">
-                                            <div class="info-icon">🧪</div>
-                                            <div class="info-text">Laboratorio<br>clínico</div>
+                                            <div class="info-icon"><i class="fa-solid fa-flask"></i></div>
+                                            <p class="fw-normal text-white fs-14p lh-1">Laboratorio<br>clínico</p>
                                         </div>
                                         <div class="col-6 col-md-6">
-                                            <div class="info-icon">🖼️</div>
-                                            <div class="info-text">Imágenes</div>
+                                            <div class="info-icon"><i class="fa-solid fa-x-ray"></i></div>
+                                            <p class="fw-normal text-white fs-14p lh-1">Imágenes</p>
                                         </div>
                                         <div class="col-6 col-md-6">
-                                            <div class="info-icon">🧘‍♂️</div>
-                                            <div class="info-text">Terapias</div>
+                                            <div class="info-icon"><i class="fa-solid fa-crutch"></i></div>
+                                            <p class="fw-normal text-white fs-14p lh-1">Terapias</p>
                                         </div>
                                         <div class="col-6 col-md-6">
-                                            <div class="info-icon">💉</div>
-                                            <div class="info-text">Procedimientos</div>
+                                            <div class="info-icon"><i class="fa-solid fa-file-prescription"></i></div>
+                                            <p class="fw-normal text-white fs-14p lh-1">Procedimientos</p>
                                         </div>
                                     </div>
                                 </div>
@@ -59,11 +59,30 @@ VerisLife - Selecciona la empresa
                     </div>
 
                     <div class="col-12 col-md-4">
-                        <div class="card shadow-1 rounded-4 h-100">
-                            <div class="card-body d-flex justify-content-center">
+                        <div class="card shadow-1 rounded-4 card-hover text-center">
+                            <div class="card-body position-relative">
                                 <img src="{{ request()->getHost() === '127.0.0.1' ? url('/') : secure_url('/') }}/assets/img/veris/parami-vertical.png" class="my-auto" alt="ParaMí" />
-                                <!-- -15% en: -->
-                                <!-- Laboratorio clínico - Imagenes - Terapias - Procedimientos -->
+                                <div class="card-info pt-5 p-4">
+                                    <div class="badge-descuento">-15% en:</div>
+                                    <div class="row g-0 justify-content-center">
+                                        <div class="col-6 col-md-6">
+                                            <div class="info-icon"><i class="fa-solid fa-flask"></i></div>
+                                            <p class="fw-normal text-white fs-14p lh-1">Laboratorio<br>clínico</p>
+                                        </div>
+                                        <div class="col-6 col-md-6">
+                                            <div class="info-icon"><i class="fa-solid fa-x-ray"></i></div>
+                                            <p class="fw-normal text-white fs-14p lh-1">Imágenes</p>
+                                        </div>
+                                        <div class="col-6 col-md-6">
+                                            <div class="info-icon"><i class="fa-solid fa-crutch"></i></div>
+                                            <p class="fw-normal text-white fs-14p lh-1">Terapias</p>
+                                        </div>
+                                        <div class="col-6 col-md-6">
+                                            <div class="info-icon"><i class="fa-solid fa-file-prescription"></i></div>
+                                            <p class="fw-normal text-white fs-14p lh-1">Procedimientos</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
