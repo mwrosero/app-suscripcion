50126db6d29f54aaa7c2c3e29d8822eaa8109e66
Michael Rosero <michael.rosero@akold.com>
2025-08-06 12:07:02 -0500

    Ajustes

--- resources/views/verislife/b2c/index.blade.php
+++ resources/views/verislife/b2c/index.blade.php
@@ -279,17 +279,6 @@
             </div>
         </div>
         <div class="row g-3 gap-4 gap-xl-0 mt-4 justify-content-between">
-            <div class="col-12 col-md-3">
-                <div class="card bg-vris-astronaut-900 border-blue-ribbon-600 rounded-4 shadow-sm text-center h-100">
-                    <div class="badge bg-white text-congress-blue-900 fs-12p position-absolute top-0 start-50 translate-middle rounded-pill px-4 py-3 small fw-medium">
-                        Hasta 15% en:
-                    </div>
-                    <div class="card-body d-flex flex-column justify-content-center align-items-center pt-4">
-                        <i class="fa-solid fa-stethoscope fs-2 text-shakespeare-300 mt-3 mb-2"></i>
-                        <p class="fs-12p fw-normal text-white mb-0 lh-1">Consultas adicionales</p>
-                    </div>
-                </div>
-            </div>
 
             <div class="col-12 col-md-5">
                 <div class="card bg-vris-astronaut-900 border-blue-ribbon-600 rounded-4 shadow-sm text-center h-100">
@@ -313,6 +302,18 @@
                             <i class="fa-solid fa-file-prescription fs-2 text-shakespeare-300 mt-3 mb-2"></i>
                             <p class="fs-12p text-white mb-0 lh-1">Procedimientos</p>
                         </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-3">
+                <div class="card bg-vris-astronaut-900 border-blue-ribbon-600 rounded-4 shadow-sm text-center h-100">
+                    <div class="badge bg-white text-congress-blue-900 fs-12p position-absolute top-0 start-50 translate-middle rounded-pill px-4 py-3 small fw-medium">
+                        Hasta 15% en:
+                    </div>
+                    <div class="card-body d-flex flex-column justify-content-center align-items-center pt-4">
+                        <i class="fa-solid fa-stethoscope fs-2 text-shakespeare-300 mt-3 mb-2"></i>
+                        <p class="fs-12p fw-normal text-white mb-0 lh-1">Consultas adicionales</p>
                     </div>
                 </div>
             </div>
@@ -336,17 +337,6 @@
             </div>
         </div>
         <div class="row g-3 gap-4 gap-xl-0 mt-4 justify-content-between">
-            <div class="col-12 col-md-3">
-                <div class="card bg-vris-astronaut-900 border-blue-ribbon-600 rounded-4 shadow-sm text-center h-100">
-                    <div class="badge bg-white text-congress-blue-900 fs-12p position-absolute top-0 start-50 translate-middle rounded-pill px-4 py-3 small fw-medium">
-                        Hasta 20% en:
-                    </div>
-                    <div class="card-body d-flex flex-column justify-content-center align-items-center pt-4">
-                        <i class="fa-solid fa-stethoscope fs-2 text-shakespeare-300 mt-3 mb-2"></i>
-                        <p class="fs-12p fw-normal text-white mb-0 lh-1">Consultas adicionales</p>
-                    </div>
-                </div>
-            </div>
 
             <div class="col-12 col-md-5">
                 <div class="card bg-vris-astronaut-900 border-blue-ribbon-600 rounded-4 shadow-sm text-center h-100">
@@ -370,6 +360,18 @@
                             <i class="fa-solid fa-file-prescription fs-2 text-shakespeare-300 mt-3 mb-2"></i>
                             <p class="fs-12p text-white mb-0 lh-1">Procedimientos</p>
                         </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-3">
+                <div class="card bg-vris-astronaut-900 border-blue-ribbon-600 rounded-4 shadow-sm text-center h-100">
+                    <div class="badge bg-white text-congress-blue-900 fs-12p position-absolute top-0 start-50 translate-middle rounded-pill px-4 py-3 small fw-medium">
+                        Hasta 20% en:
+                    </div>
+                    <div class="card-body d-flex flex-column justify-content-center align-items-center pt-4">
+                        <i class="fa-solid fa-stethoscope fs-2 text-shakespeare-300 mt-3 mb-2"></i>
+                        <p class="fs-12p fw-normal text-white mb-0 lh-1">Consultas adicionales</p>
                     </div>
                 </div>
             </div>
