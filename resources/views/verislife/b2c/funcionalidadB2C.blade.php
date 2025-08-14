49ec709cc2d1374a5c7306bbbf27cba94a468487
miguelangel <miguelangpoveda@gmail.com>
2025-08-13 21:42:15 -0500

    APP-123 Ajuste de disenio landing page b2c home

--- resources/views/verislife/b2c/funcionalidadB2C.blade.php
+++ resources/views/verislife/b2c/funcionalidadB2C.blade.php
@@ -80,8 +80,8 @@
             <div class="card card-transition border-perano-300 rounded-3 shadow-sm p-3 h-100">
     			<img src="${logoSrc}" class="img-fluid mx-auto mb-3" alt="${detalleSuscripcion.lineaNegocio}" width="128">
                 <div class="w-100 box-img-plan rounded" style="background: url(${url_site}/assets/img/veris/planes/${imgFeatured}) no-repeat top center; background-size: cover; height: 165px;"></div>
-                <h5 class="bg-zumthor-50 text-blue-zodiac-950 fw-medium text-start px-3 py-3 position-relative rounded" style="margin-top:-25px;">
-                    ${detalle.nombre}
+                <h5 class="bg-zumthor-50 text-blue-zodiac-950 text-capitalize fw-medium text-start px-3 py-3 position-relative rounded" style="margin-top:-25px;">
+                    ${capitalizeWords(detalle.nombre)}
                     <div class="position-absolute">
                         <span class="badge rounded-pill bg-blue-ribbon-600 text-white fw-normal" style="font-size: 0.625rem;">AHORRA ${detalle.porcentajeDescuento}%</span>
                     </div>
@@ -150,4 +150,12 @@
             </div>
         </div>`;
     }
+
+    function capitalizeWords(str) {
+        return str
+            .toLowerCase()
+            .replace(/\b\w/g, char => char.toUpperCase());
+    }
+
+
 </script>
