dc5888100c2e289d5a3edd7f135c6960bda6ed36
Michael Rosero <michael.rosero@akold.com>
2025-10-16 23:01:51 -0500

    Ajuste aprobacion Cliente

--- resources/views/template/login.blade.php
+++ resources/views/template/login.blade.php
@@ -22,6 +22,14 @@
   <!-- Favicon -->
   <link rel="icon" type="image/x-icon" href="{{ request()->getHost() === '127.0.0.1' ? url('/') : secure_url('/') }}/assets/img/favicon/favicon.ico" />
 
+  <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-M6Q6MQ3Q');</script>
+    <!-- End Google Tag Manager -->
+
   <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -62,6 +70,10 @@
 </head>
 
 <body class="bg-white">
+  <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M6Q6MQ3Q"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
   <!-- Content -->
 
   <div class="container-xxl">
