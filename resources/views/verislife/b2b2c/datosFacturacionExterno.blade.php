95449cd1cb00a25cf4c937a1b8b7812cb1d5bb0f
Michael Rosero <michael.rosero@akold.com>
2025-10-17 16:35:00 -0500

    Ajuste
    target="_blank"

--- resources/views/verislife/b2b2c/datosFacturacionExterno.blade.php
+++ resources/views/verislife/b2b2c/datosFacturacionExterno.blade.php
@@ -470,8 +470,8 @@ Veris Care - Suscripción
                                                 <div class="col-md-12 col-xl-8">
                                                     <div class="form-check">
                                                         <input class="form-check-input" type="checkbox" id="privacy" name="privacy">
-                                                        <label class="form-check-label fs-10p" for="privacy">
-                                                            He leído y comprendo la autorización para el <a href="https://www.veris.com.ec/politicas/" class="text-mariner-600 text-decoration-underline" nemonico-rel="TRATAMIENTO_DATOS">Tratamiento de mis datos personales</a> <span class="text-danger">*</span>
+                                                        <label class="form-check-label fs-10p">
+                                                            He leído y comprendo la autorización para el <a href="https://www.veris.com.ec/politicas/" target="_blank" class="text-mariner-600 text-decoration-underline" nemonico-rel="TRATAMIENTO_DATOS">Tratamiento de mis datos personales</a> <span class="text-danger">*</span>
                                                         </label>
                                                     </div>
                                                 </div>
