27ea4f913a3bcada45ebcb29f0f56c820abe1337
Michael Rosero <michael.rosero@akold.com>
2025-12-03 16:41:13 -0500

    Ajustes

--- resources/views/verislife/dependiente/registro.blade.php
+++ resources/views/verislife/dependiente/registro.blade.php
@@ -1219,11 +1219,11 @@ Registro
             return false;
         }
 
-        let existeWS = await existeIdentificionSuscrita(numero);
+        {{-- let existeWS = await existeIdentificionSuscrita(numero);
         if(existeWS){
             showMessage('warning','Atención','Paciente con esa identificación ya se encuentra suscrito');
             return false;
-        }
+        } --}}
         
         try {
             const baseUrl = `${api_url}/general/v1/util/validar_identificacion`;
